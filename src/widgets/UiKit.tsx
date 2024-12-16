fcafaa7cda3395812cc433acff81458d3082d6a1
khalid <shkhalid95@mail.ru>
2024-12-16 17:12:35 +0300

    init

---
+++ src/widgets/UiKit.tsx
@@ -0,0 +1,11 @@
+import { Button } from '~/shared/ui'
+
+export function UiKit() {
+	return (
+		<div>
+			<Button variant="primary" className="custom-shadow">
+				Click Me
+			</Button>
+		</div>
+	)
+}
